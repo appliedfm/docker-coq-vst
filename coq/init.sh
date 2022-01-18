a6cea48f745a3eb68933e52e4595fe478f1d5087
Timothy Carstens <intoverflow@gmail.com>
2022-01-18 13:59:46 -0800

    Add doxygen

--- coq/init.sh
+++ coq/init.sh
@@ -2,4 +2,5 @@
 
 sudo apt-get update -y -q               || exit -1
 sudo apt-get install -y -q tree         || exit -1
+sudo apt-get install -y -q doxygen      || exit -1
 sudo apt-get install -y -q gcc-multilib || exit -1
