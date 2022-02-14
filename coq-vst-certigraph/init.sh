c218eeacf9014f6c29ded454642d126514d2c810
Timothy Carstens <intoverflow@gmail.com>
2022-02-13 22:41:33 -0800

    Update CertiGraph URL

--- coq-vst-certigraph/init.sh
+++ coq-vst-certigraph/init.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 eval $(opam env "--switch=${COMPILER}" --set-switch)
-git clone https://github.com/Salamari/CertiGraph.git ~/CertiGraph
+git clone https://github.com/CertiGraph/CertiGraph.git ~/CertiGraph
 opam pin -n -y ~/CertiGraph
 opam install -y -v -j "${NJOBS}" coq-certigraph coq-certigraph-32
-rm -rf ~/CertiGraph
 opam clean -a -c -s --logs
 opam config list && opam list
