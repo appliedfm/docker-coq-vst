1ffac427108c85e24df563cf8b25c0a364b07df4
Timothy Carstens <intoverflow@gmail.com>
2022-02-14 01:40:08 -0800

    Fix tags for coq_vst and coq_vst_certigraph

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ build__coq_vst() {
         --build-arg BASE_TAG="${BASE_TAG}" \
         --build-arg COMPCERT_VERSION="${COMPCERT_VERSION}" \
         --build-arg VST_VERSION="${VST_VERSION}" \
-        -t "appliedfm/${IMAGE}:${BASE_TAG}--compcert-${COMPCERT_VERSION}" \
+        -t "appliedfm/${IMAGE}:${BASE_TAG}--vst-${VST_VERSION}--compcert-${COMPCERT_VERSION}" \
         ${IMAGE}
 }
 
@@ -51,6 +51,7 @@ build__coq_vst_certigraph() {
     local BASE_TAG=$1
     local COMPCERT_VERSION=$2
     local VST_VERSION=$3
+    local CERTIGRAPH_VERSION=$3
     local SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
     docker build \
         --no-cache \
@@ -58,6 +59,6 @@ build__coq_vst_certigraph() {
         --build-arg BASE_TAG="${BASE_TAG}" \
         --build-arg COMPCERT_VERSION="${COMPCERT_VERSION}" \
         --build-arg VST_VERSION="${VST_VERSION}" \
-        -t "appliedfm/${IMAGE}:${BASE_TAG}--compcert-${COMPCERT_VERSION}" \
+        -t "appliedfm/${IMAGE}:${BASE_TAG}--certigraph-${CERTIGRAPH_VERSION}--vst-${VST_VERSION}--compcert-${COMPCERT_VERSION}" \
         ${IMAGE}
 }
